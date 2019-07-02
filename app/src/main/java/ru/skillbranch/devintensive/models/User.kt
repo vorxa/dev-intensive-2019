ddcc5f1f0f6be2c8df62f84083013e539adf00c8
vorxa <vorxa@yandex.ru>
2019-07-02 20:26:31 +0700

    Builder implemented

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -40,17 +40,34 @@ data class User (
         }
     }
 
-    data class Builder(
-        var id:String,
-        var firstName:String?,
-        var lastName:String?,
-        var avatar:String?,
-        var rating:Int = 0,
-        var respect:Int = 0,
-        var lastVisit: Date? = null,
-        var isOnline:Boolean = false
+    private constructor(builder: Builder) : this(builder.id,
+        builder.firstName,
+        builder.lastName,
+        builder.avatar,
+        builder.rating,
+        builder.respect,
+        builder.lastVisit,
+        builder.isOnline)
 
-    ) {
+    class Builder {
+        var id:String = "0"
+            private set
+        var firstName:String? = null
+            private set
+        var lastName:String? = null
+            private set
+        var avatar:String? = null
+            private set
+        var rating:Int = 0
+            private set
+        var respect:Int = 0
+            private set
+        var lastVisit: Date? = null
+            private set
+        var isOnline:Boolean = false
+            private set
+
+
         fun id(id: String) = apply { this.id = id }
         fun firstName(firstName: String) = apply { this.firstName = firstName }
         fun lastName(lastName: String) = apply { this.lastName = lastName }
@@ -59,7 +76,7 @@ data class User (
         fun respect(respect: Int) = apply { this.respect = respect}
         fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit}
         fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline}
-        fun build() = User(id, firstName, lastName)
+        fun build() = User(this)
 
     }
 }
